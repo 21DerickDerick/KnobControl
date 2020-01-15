8f8595d5378fedcb3c2c2533182c1923d27ab4bd
Derick <21DerickDerick@gmail.com>
2020-01-15 13:25:24 +0800

    Initial Commit

---
+++ KnobControl/KnobControl.h
@@ -0,0 +1,19 @@
+//
+//  KnobControl.h
+//  KnobControl
+//
+//  Created by Derick on 15/1/20.
+//  Copyright © 2020 DerickProductions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for KnobControl.
+FOUNDATION_EXPORT double KnobControlVersionNumber;
+
+//! Project version string for KnobControl.
+FOUNDATION_EXPORT const unsigned char KnobControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KnobControl/PublicHeader.h>
+
+
